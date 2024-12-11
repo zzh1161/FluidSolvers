55d148fd78e387fe9799c12ca564cd8b51513709
zzh <1563833973@qq.com>
2024-12-11 20:19:44 +0800

    add leapfrog

--- CovectorFluids/taichi_code/CF_leapfrog.py
+++ CovectorFluids/taichi_code/CF_leapfrog.py
@@ -4,5 +4,104 @@ import numpy as np
 
 @ti.data_oriented
 class Leapfrog:
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        a=0.02,
+        dist_a=1.5,
+        dist_b=3.0,
+        res=256,
+        max_jacobi_iter=10000
+    ):
+        self.a = a
+        self.dist_a = dist_a
+        self.dist_b = dist_b
+        self.nx = res
+        self.ny = res
+        self.h = 2 * ti.math.pi / res
+        self.max_jacobi_iter = max_jacobi_iter
+        self.max_curl = ti.field(dtype=ti.f32, shape=())
+
+        self.curl = ti.field(dtype=ti.f32, shape=(self.nx+1, self.ny+1))
+        self.pressure = ti.field(dtype=ti.f32, shape=(self.nx+1, self.ny+1))
+        self.pressure_temp = ti.field(dtype=ti.f32, shape=(self.nx+1, self.ny+1))
+        self.velx = ti.field(dtype=ti.f32, shape=(self.nx+1, self.ny))
+        self.vely = ti.field(dtype=ti.f32, shape=(self.nx, self.ny+1))
+        self.vor = ti.field(dtype=ti.f32, shape=(self.nx+1, self.ny+1))
+
+        self.pressure.fill(0)
+        self.pressure_temp.fill(0)
+        self.vor.fill(0)
+
+    @ti.kernel
+    def init_curl(self):
+        for i, j in self.curl:
+            pos = ti.Vector([i, j]) * self.h - ti.Vector([ti.math.pi, ti.math.pi])
+            vor_pos0 = ti.Vector([-0.5 * self.dist_a, -2.0])
+            vor_pos1 = ti.Vector([ 0.5 * self.dist_a, -2.0])
+            vor_pos2 = ti.Vector([-0.5 * self.dist_b, -2.0])
+            vor_pos3 = ti.Vector([ 0.5 * self.dist_b, -2.0])
+            r_sqr0 = (pos - vor_pos0).norm_sqr()
+            r_sqr1 = (pos - vor_pos1).norm_sqr()
+            r_sqr2 = (pos - vor_pos2).norm_sqr()
+            r_sqr3 = (pos - vor_pos3).norm_sqr()
+            c_a =  1000.0/(2*ti.math.pi) * ti.exp(-r_sqr0/(2*self.a**2))
+            c_b = -1000.0/(2*ti.math.pi) * ti.exp(-r_sqr1/(2*self.a**2))
+            c_c =  1000.0/(2*ti.math.pi) * ti.exp(-r_sqr2/(2*self.a**2))
+            c_d = -1000.0/(2*ti.math.pi) * ti.exp(-r_sqr3/(2*self.a**2))
+            self.curl[i,j] = c_a + c_b + c_c + c_d
+            self.max_curl[None] = ti.max(self.max_curl[None], self.curl[i,j])
+
+    @ti.kernel
+    def poisson_jacobi(self):
+        for i, j in self.pressure:
+            # periodic boundary condition
+            # pl = self.pressure[i-1, j] if i > 0 else self.pressure[self.nx, j]
+            # pr = self.pressure[i+1, j] if i < self.nx else self.pressure[0, j]
+            # pb = self.pressure[i, j-1] if j > 0 else self.pressure[i, self.ny]
+            # pt = self.pressure[i, j+1] if j < self.ny else self.pressure[i, 0]
+
+            # Dirichlet boundary condition with zero boundary value
+            pl = self.pressure[i-1, j] if i > 0 else 0
+            pr = self.pressure[i+1, j] if i < self.nx else 0
+            pb = self.pressure[i, j-1] if j > 0 else 0
+            pt = self.pressure[i, j+1] if j < self.ny else 0
+
+            self.pressure_temp[i,j] = 0.25 * (pl+pr+pb+pt - self.h**2 * self.curl[i,j])
+
+    def pressure_solve(self):
+        for _ in range(self.max_jacobi_iter):
+            self.poisson_jacobi()
+            self.pressure.copy_from(self.pressure_temp)
+
+    @ti.kernel
+    def get_velocity(self):
+        for i, j in self.velx:
+            self.velx[i,j] = -(self.pressure[i,j+1] - self.pressure[i,j]) / self.h
+        for i, j in self.vely:
+            self.vely[i,j] = (self.pressure[i+1,j] - self.pressure[i,j]) / self.h
+
+    def generate_field(self):
+        self.init_curl()
+        self.pressure_solve()
+        self.get_velocity()
+
+    @ti.kernel
+    def vel_to_vor(self):
+        for i, j in ti.ndrange((1, self.nx), (1, self.ny)):
+            self.vor[i,j] = (self.vely[i,j] - self.vely[i-1,j] - self.velx[i,j] + self.velx[i,j-1]) / self.h
+
+# ti.init(arch=ti.gpu)
+# leapfrog = Leapfrog()
+# leapfrog.generate_field()
+# leapfrog.vel_to_vor()
+
+# plt.figure(figsize=(12, 6))
+# plt.subplot(1,2,1)
+# plt.imshow(leapfrog.curl.to_numpy(), cmap='jet')
+# plt.colorbar()
+# plt.title('curl')
+# plt.subplot(1,2,2)
+# plt.imshow(leapfrog.vor.to_numpy(), cmap='jet')
+# plt.colorbar()
+# plt.title('vorticity')
+# plt.show()
--- CovectorFluids/taichi_code/CF_main.py
+++ CovectorFluids/taichi_code/CF_main.py
@@ -1,9 +1,25 @@
 import taichi as ti
 import time
+import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 from CF_taylor import TaylorGreenVortex
+from CF_leapfrog import Leapfrog
 from CF_solver import CovectorFluidSolver
 from CF_solver import Scheme
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--demo',
+                    '-d',
+                    type=int,
+                    default=0,
+                    help='0: Taylor-Green vortex, 1: Leapfrog')
+parser.add_argument('--scheme',
+                    '-s',
+                    type=int,
+                    default=3,
+                    help='1: CF+SL_1ST, 2: CF+SL_2ND, 3: CF+BFECC, 4: CF+MCM_2ND')
+args, unknowns = parser.parse_known_args()
 
 ti.init(arch=ti.gpu)
 
@@ -12,66 +28,85 @@ ny = 256
 L = 2*ti.math.pi
 h = L/nx
 dt = 0.025
-total_frame = 500
+total_frame = 600 if args.demo == 0 else 2000 if args.demo == 1 else 0
+cur_frame = 0
 
 taylorVor = TaylorGreenVortex(res=nx)
+leapfrog = Leapfrog(res=nx)
 solver_1 = CovectorFluidSolver(nx, ny, h, dt, scheme=Scheme.CF_SL_1ST)
 solver_2 = CovectorFluidSolver(nx, ny, h, dt, scheme=Scheme.CF_SL_2ND)
 solver_b = CovectorFluidSolver(nx, ny, h, dt, scheme=Scheme.CF_BFECC)
 solver_m = CovectorFluidSolver(nx, ny, h, dt, scheme=Scheme.CF_MCM_2ND)
 
-taylorVor.generate_field()
-solver_1.init_velocity(taylorVor.velx, taylorVor.vely)
-solver_2.init_velocity(taylorVor.velx, taylorVor.vely)
-solver_b.init_velocity(taylorVor.velx, taylorVor.vely)
-solver_m.init_velocity(taylorVor.velx, taylorVor.vely)
+if args.demo == 0:
+    taylorVor.generate_field()
+    solver_1.init_velocity(taylorVor.velx, taylorVor.vely)
+    solver_2.init_velocity(taylorVor.velx, taylorVor.vely)
+    solver_b.init_velocity(taylorVor.velx, taylorVor.vely)
+    solver_m.init_velocity(taylorVor.velx, taylorVor.vely)
+elif args.demo == 1:
+    leapfrog.generate_field()
+    solver_1.init_velocity(leapfrog.velx, leapfrog.vely)
+    solver_2.init_velocity(leapfrog.velx, leapfrog.vely)
+    solver_b.init_velocity(leapfrog.velx, leapfrog.vely)
+    solver_m.init_velocity(leapfrog.velx, leapfrog.vely)
 
-# gui = ti.GUI('Taylor Green Vortex', (nx+1, ny+1))
-# cur_frame = 0
-# timer = 0
-# while cur_frame < total_frame:
-#     for e in gui.get_events(ti.GUI.PRESS):
-#         if e.key in [ti.GUI.ESCAPE, ti.GUI.EXIT]:
-#             exit()
-#     solver_m.advance()
-#     gui.set_image(solver_m.abs_vor)
-#     delta_t = time.time() - timer
-#     timer = time.time()
-#     gui.text(content=f'fps: {(1.0/delta_t):.1f}', pos=(0,0.98), color=0xffaa77)
-#     gui.show()
-#     cur_frame += 1
-
-cur_frame = 0
-vmin = 0.0
-vmax = 10.0
+timer = 0.0
+gui = ti.GUI('Taylor Green Vortex', (nx+1, ny+1)) if args.demo == 0 \
+      else ti.GUI('Leapfrog', (nx+1, ny+1)) if args.demo == 1 \
+      else None
 while cur_frame < total_frame:
-    solver_1.advance()
-    solver_2.advance()
-    solver_b.advance()
-    solver_m.advance()
-
-    plt.close()
-    fig, axs = plt.subplots(2, 2, figsize=(8, 8))
-    # subplot 1
-    im1 = axs[0, 0].imshow(solver_1.abs_vor.to_numpy().transpose(), cmap='jet', vmin=vmin, vmax=vmax)
-    axs[0, 0].set_title('1st order Semi-Lagrangian')
-    axs[0, 0].axis('off')
-    # subplot 2
-    axs[0, 1].imshow(solver_2.abs_vor.to_numpy().transpose(), cmap='jet', vmin=vmin, vmax=vmax)
-    axs[0, 1].set_title('2nd order Semi-Lagrangian')
-    axs[0, 1].axis('off')
-    # subplot 3
-    axs[1, 0].imshow(solver_b.abs_vor.to_numpy().transpose(), cmap='jet', vmin=vmin, vmax=vmax)
-    axs[1, 0].set_title('CF+BFECC')
-    axs[1, 0].axis('off')
-    # subplot 4
-    axs[1, 1].imshow(solver_m.abs_vor.to_numpy().transpose(), cmap='jet', vmin=vmin, vmax=vmax)
-    axs[1, 1].set_title('CF+MCM')
-    axs[1, 1].axis('off')
-    # unified colorbar
-    cbar = fig.colorbar(im1, ax=axs, orientation='vertical', fraction=0.02, pad=0.04)
-    # save image
-    plt.savefig(f'./output/{cur_frame}.png')
-    cbar.remove()
-
+    for e in gui.get_events(ti.GUI.PRESS):
+        if e.key in [ti.GUI.ESCAPE, ti.GUI.EXIT]:
+            exit()
+    if args.scheme == 0:
+        solver_1.advance()
+        gui.set_image(solver_1.abs_vor)
+    elif args.scheme == 1:
+        solver_2.advance()
+        gui.set_image(solver_2.abs_vor)
+    elif args.scheme == 2:
+        solver_b.advance()
+        gui.set_image(solver_b.abs_vor)
+    elif args.scheme == 3:
+        solver_m.advance()
+        gui.set_image(solver_m.abs_vor)
+    delta_t = time.time() - timer
+    timer = time.time()
+    gui.text(content=f'fps: {(1.0/delta_t):.1f}', pos=(0,0.98), color=0xffaa77)
+    gui.show()
     cur_frame += 1
+
+# vmin = 0.0
+# vmax = 10.0
+# while cur_frame < total_frame:
+#     solver_1.advance()
+#     solver_2.advance()
+#     solver_b.advance()
+#     solver_m.advance()
+
+#     plt.close()
+#     fig, axs = plt.subplots(2, 2, figsize=(8, 8))
+#     # subplot 1
+#     im1 = axs[0, 0].imshow(np.rot90(solver_1.abs_vor.to_numpy()), cmap='jet', vmin=vmin, vmax=vmax)
+#     axs[0, 0].set_title('1st order Semi-Lagrangian')
+#     axs[0, 0].axis('off')
+#     # subplot 2
+#     axs[0, 1].imshow(np.rot90(solver_2.abs_vor.to_numpy()), cmap='jet', vmin=vmin, vmax=vmax)
+#     axs[0, 1].set_title('2nd order Semi-Lagrangian')
+#     axs[0, 1].axis('off')
+#     # subplot 3
+#     axs[1, 0].imshow(np.rot90(solver_b.abs_vor.to_numpy()), cmap='jet', vmin=vmin, vmax=vmax)
+#     axs[1, 0].set_title('CF+BFECC')
+#     axs[1, 0].axis('off')
+#     # subplot 4
+#     axs[1, 1].imshow(np.rot90(solver_m.abs_vor.to_numpy()), cmap='jet', vmin=vmin, vmax=vmax)
+#     axs[1, 1].set_title('CF+MCM')
+#     axs[1, 1].axis('off')
+#     # unified colorbar
+#     cbar = fig.colorbar(im1, ax=axs, orientation='vertical', fraction=0.02, pad=0.04)
+#     # save image
+#     plt.savefig(f'./output/{cur_frame}.png')
+#     cbar.remove()
+
+#     cur_frame += 1
